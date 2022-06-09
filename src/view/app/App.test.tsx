7b6231a1583097a31a8a2857a5c59d1779279aac
Liana <liana.nikitina98@gmail.com>
2022-06-09 17:04:02 +0300

    clear files

--- src/view/app/App.test.tsx
+++ src/view/app/App.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
-import { store } from "../../app/store";
+import { store } from "../../store/store";
 import { App } from "./App";
 import { MemoryRouter } from "react-router-dom";
 
